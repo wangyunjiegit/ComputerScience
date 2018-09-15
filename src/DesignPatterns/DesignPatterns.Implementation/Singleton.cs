e3bc3eb66a1c4fa42de14834bfebe3d341e7929d
wangyunjie <wangyunjie@ninetowns.com>
2018-09-16 01:44:50 +0800

    基础类库初始化

---
+++ src/DesignPatterns/DesignPatterns.Implementation/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using Infrastructure.Log.Concrete;
+using System;
+
+namespace DesignPatterns.Implementation
+{
+    /// <summary>
+    /// 单例:确保一个类只有一个实例，并提供该实例的全局访问点。    
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton uniqueInstance;
+
+        private Singleton() { }
+
+        public static Singleton GetUniqueInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new Singleton();
+
+                new LogHelper().WriteLog("testtt");
+            }
+
+            return uniqueInstance;
+        }
+    }
+}
---
+++ src/DesignPatterns/DesignPatterns.UnitTest/SingletonTest.cs
@@ -0,0 +1,25 @@
+using DesignPatterns.Implementation;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DesignPatterns.UnitTest
+{
+    /// <summary>
+    /// 单例测试:确保一个类只有一个实例，并提供该实例的全局访问点。
+    /// </summary>
+    public class SingletonTest
+    {
+        [Fact]
+        public void Main()
+        {
+            for (int i = 0; i < 1000; i++)
+            {                
+                Task.Run(() => {
+                    var instance = Singleton.GetUniqueInstance();
+                    var pause = "this is pause";
+                });
+            }            
+        }
+    }
+}
